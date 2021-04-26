e94c325650af2187df4b452a23b0558540a08a9a
raymondtran95 <>
2021-04-26 12:35:42 +0000

    comments

--- todo.py
+++ todo.py
@@ -2,37 +2,47 @@
 from todo import createTodo, readListAll, deleteAll, readListSubstring, readListComplete, readListNotComplete,toggleStatus, deleteMultiple, getCommandArguments, updateDescription, get_cli
 
 if __name__ == "__main__":
+	# Create TODO item(s)
 	if get_cli().command[0] == 'create':
 		x = getCommandArguments(get_cli().command)
 		createTodo(x)
 	
+	# List item(s)
 	if get_cli().command[0] == 'list-all':
+		# Handle --substring CLI argument
 		if get_cli().substring:
 			readListSubstring(get_cli().substring[0])
+		# Handle --no-complete CLI argument
 		elif get_cli().no_complete:
 			readListNotComplete(get_cli().no_complete[0])
+		# Handle --complete CLI argument
 		elif get_cli().complete:
 			readListComplete(get_cli().complete[0])
 		else:
+			# Handle 'list-all'
 			if len(get_cli().command) == 1:
 				print('All TODO items below:')
 				readListAll()
 			# Catch for inputs after list-all
 			else:
 				print('Do not include argument after "list-all"')
-		
+	
+	# Delete all TODOs in todo.json
 	if get_cli().command[0] == 'delete-all':
 		deleteAll()
 		print('All TODO items have been deleted')
 	
+	# Toggle 'completion_status' in all TODOs
 	if get_cli().command[0] == 'toggle':
 		x = getCommandArguments(get_cli().command)
 		toggleStatus(x)
-		
+	
+	# Delete TODO item(s)
 	if get_cli().command[0] == 'delete':
 		x = getCommandArguments(get_cli().command)
 		deleteMultiple(x)
 
+	# Update a TODO item description
 	if get_cli().command[0] == 'update':
 		x = getCommandArguments(get_cli().command)
 		# Checks for only 2 inputs
